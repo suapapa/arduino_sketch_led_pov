cd6b0e60721fbfab1998f2a5d221b51f51e31d1d
Homin Lee <homin.lee@suapapa.net>
2012-06-05 11:04:16 +0900

    Initial import of LED_POV_F

---
+++ LED_POV_F.ino
@@ -0,0 +1,195 @@
+byte FontFile[] = {
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xC0, 0x03,	//OO............OO
+  0xC0, 0x03,	//OO............OO
+  0xDF, 0x7F,	//OO.OOOOO.OOOOOOO
+  0xDF, 0x7F,	//OO.OOOOO.OOOOOOO
+  0xDF, 0x7F,	//OO.OOOOO.OOOOOOO
+  0xDF, 0x7F,	//OO.OOOOO.OOOOOOO
+  0xCE, 0x3F,	//OO..OOO...OOOOOO
+  0xE0, 0x83,	//OOO.....O.....OO
+  0xF1, 0xC3,	//OOOO...OOO....OO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xF8, 0x1F,	//OOOOO......OOOOO
+  0xF0, 0x0F,	//OOOO........OOOO
+  0xE7, 0xE7,	//OOO..OOOOOO..OOO
+  0xCF, 0xF3,	//OO..OOOOOOOO..OO
+  0xDF, 0xFB,	//OO.OOOOOOOOOO.OO
+  0xDF, 0xFB,	//OO.OOOOOOOOOO.OO
+  0xDF, 0xFB,	//OO.OOOOOOOOOO.OO
+  0xCF, 0xF3,	//OO..OOOOOOOO..OO
+  0xE7, 0xE7,	//OOO..OOOOOO..OOO
+  0xF0, 0x0F,	//OOOO........OOOO
+  0xF8, 0x1F,	//OOOOO......OOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xF8, 0x1F,	//OOOOO......OOOOO
+  0xF0, 0x0F,	//OOOO........OOOO
+  0xE7, 0xE7,	//OOO..OOOOOO..OOO
+  0xCF, 0xF3,	//OO..OOOOOOOO..OO
+  0xDF, 0xFB,	//OO.OOOOOOOOOO.OO
+  0xDF, 0xFB,	//OO.OOOOOOOOOO.OO
+  0xDF, 0xFB,	//OO.OOOOOOOOOO.OO
+  0xCF, 0xF3,	//OO..OOOOOOOO..OO
+  0xE7, 0xE7,	//OOO..OOOOOO..OOO
+  0xF7, 0xEF,	//OOOO.OOOOOO.OOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xC0, 0x03,	//OO............OO
+  0xC0, 0x03,	//OO............OO
+  0xFE, 0x7F,	//OOOOOOO..OOOOOOO
+  0xFC, 0x7F,	//OOOOOO...OOOOOOO
+  0xF9, 0x3F,	//OOOOO..O..OOOOOO
+  0xF3, 0x9F,	//OOOO..OOO..OOOOO
+  0xE7, 0xCF,	//OOO..OOOOO..OOOO
+  0xCF, 0xE7,	//OO..OOOOOOO..OOO
+  0xDF, 0xF3,	//OO.OOOOOOOOO..OO
+  0xFF, 0xFB,	//OOOOOOOOOOOOO.OO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xC0, 0x13,	//OO.........O..OO
+  0xC0, 0x13,	//OO.........O..OO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xC0, 0x13,	//OO.........O..OO
+  0xC0, 0x13,	//OO.........O..OO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF,	//OOOOOOOOOOOOOOOO
+  0xFF, 0xFF 	//OOOOOOOOOOOOOOOO
+};
+
+
+unsigned long time;
+unsigned long new_time;
+unsigned long old_time;
+unsigned long dp_time;
+
+unsigned long wait_time;
+unsigned long data_end;
+unsigned long data_start;
+unsigned int data_size;
+
+void setup()
+{
+  Serial.begin(115200);
+  PORTB = 0x3F;
+  PORTC = 0x3F;
+  PORTD = 0xF0;
+
+  DDRB = 0x3F;
+  DDRC = 0x3F;
+  DDRD = 0xF0;
+
+  Serial.println(sizeof(FontFile) / 2);
+  data_size = sizeof(FontFile);
+}
+
+void loop()
+{
+  if ((PIND & 0x0C) == 0x08) {
+    delay(40);
+    new_time = micros();
+    time = new_time - old_time;
+    old_time = new_time;
+    wait_time = time / 2 - ((data_end - data_start) / 2) ;
+
+    while (micros() < (new_time + wait_time)) if ((PIND & 0x0C) == 0x04)  break ;
+
+    //delay(wait_time/1000);
+
+    data_start = micros();
+    DP_L();
+    data_end = micros();
+    while (!((PIND & 0x0C) == 0x04)) ;
+  } else if ((PIND & 0x0C) == 0x04) {
+    delay(40);
+    new_time = micros();
+    time = new_time - old_time;
+    old_time = new_time;
+    wait_time = time / 2 - ((data_end - data_start) / 2) ;
+
+    while (micros() < (new_time + wait_time)) if ((PIND & 0x0C) == 0x08)  break ;
+
+    //delay(wait_time/1000);
+
+    data_start = micros();
+    DP_R();
+    data_end = micros();
+    while (!((PIND & 0x0C) == 0x08)) ;
+  }
+
+}
+
+void DP_R()
+{
+  for (int i = 0; i < data_size; i += 2) {
+    if ((PIND & 0x0C) == 0x08) {
+      PORTD = ~0;
+      PORTB = ~0;
+      PORTC = ~0;
+      return;
+    }
+    PORTD = FontFile[i + 1] << 4;         // PD4 : lsb
+    PORTB = ((FontFile[i] << 4) & 0x30)  | ((FontFile[i + 1] >> 4) & 0x0F);
+    PORTC = (FontFile[i] >> 2) & 0x3F;     // PC5 : msb
+    delay(1);
+    //delayMicroseconds(700);
+
+  }
+  PORTD = ~0;
+  PORTB = ~0;
+  PORTC = ~0;
+}
+
+void DP_L()
+{
+  for (int i = data_size - 2; i > 0; i -= 2) {
+    if ((PIND & 0x0C) == 0x04) {
+      PORTD = ~0;
+      PORTB = ~0;
+      PORTC = ~0;
+      return;
+    }
+    PORTD = FontFile[i + 1] << 4;         // PD4 : lsb
+    PORTB = ((FontFile[i] << 4) & 0x30)  | ((FontFile[i + 1] >> 4) & 0x0F);
+    PORTC = (FontFile[i] >> 2) & 0x3F;     // PC5 : msb
+    delay(1);
+    //delayMicroseconds(700);
+  }
+  PORTD = ~0;
+  PORTB = ~0;
+  PORTC = ~0;
+}
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# LED POV
+
+Original source written by "워리님" who is master of "Arduino Story",
+Korean No. 1 Arduino community.
+
+
+
